36a358a880c83b519405c4d5a3f90e9b1ba50772
Charmika17 <138001776+Charmika17@users.noreply.github.com>
2024-09-07 18:56:38 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "84bc6c29492e1c9c18cc7fc81d930493",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
